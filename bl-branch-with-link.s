911c1e7decb0bb6e35bfda59a6a579baa70168d5
JD Shaffer <35787312+jdshaffer@users.noreply.github.com>
2025-06-15 21:16:33 +0900

    Update bl-branch-with-link.s
    Somehow my code alignment was all goofy, tabs and spaces mixed. Fixed the indents to to make it pretty.

--- bl-branch-with-link.s
+++ bl-branch-with-link.s
@@ -27,30 +27,30 @@
 main:
     BL    branch_1                    // Branch to branch_1 (with a link to return here to main)
     BL    branch_2                    // Branch to branch_2 (with a link to return here to main)
-	B     end                         // Branch to end      (no need to return to here, so no link)
+    B     end                         // Branch to end      (no need to return to here, so no link)
 
 
 branch_1:
-	ADRP  X0, output_1@PAGE           // load the string in output_1 to print
-	ADD   X0, X0, output_1@PAGEOFF
-	B 	_printf                       // print the loaded message
+    ADRP  X0, output_1@PAGE           // load the string in output_1 to print
+    ADD   X0, X0, output_1@PAGEOFF
+    B     _printf                     // print the loaded message
     RET                               // return to where we left off in "main"
 
 
 branch_2:
-	ADRP  X0, output_2@PAGE           // load the string in output_2 to print
-	ADD   X0, X0, output_2@PAGEOFF
-	B 	_printf                       // print the loaded message
-    RET                               // return to where we left off in "main"
+   ADRP  X0, output_2@PAGE            // load the string in output_2 to print
+   ADD   X0, X0, output_2@PAGEOFF
+   B     _printf                      // print the loaded message
+   RET                                // return to where we left off in "main"
 
 
 end:
-	ADRP  X0, output_3@PAGE           // load the string in output_3 to print
-	ADD   X0, X0, output_3@PAGEOFF
-	BL 	_printf                       // print the loaded message
+   ADRP  X0, output_3@PAGE            // load the string in output_3 to print
+   ADD   X0, X0, output_3@PAGEOFF
+   BL    _printf                      // print the loaded message
 
-	mov   X16, 1                      // System call to terminate this program
-	svc   0
+   mov   X16, 1                       // System call to terminate this program
+   svc   0
 
 
 .data
